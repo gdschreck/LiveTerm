fda28a60433522499a4659f11770750a13bf6eea
Galen Schreck <galen.schreck@gmail.com>
2025-03-22 18:21:47 -0400

    more config updates
    updated hostname
    fixed paypal links
    updated initial menu to replace github with linkedin
    attempting to add strava to config file and sumfetch

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -160,6 +160,6 @@ export const banner = (args?: string[]): string => {
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+Type 'Linkedin' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.linkedin_url}" target="_blank">here</a></u> for more about me.
 `;
 };
