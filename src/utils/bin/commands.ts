134fb3dd93f718243a346c9e623686f9f2d517d7
Galen Schreck <galen.schreck@gmail.com>
2025-03-22 21:53:52 -0400

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -159,6 +159,6 @@ export const banner = (args?: string[]): string => {
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
-Type 'Linkedin' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.linkedin_url}" target="_blank">here</a></u> for more about me.
+Type 'linkedin' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.linkedin_url}" target="_blank">here</a></u> for more about me.
 `;
 };
