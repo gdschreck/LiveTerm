9081a70d006530606d04192e855de33d4f8c7b98
Galen Schreck <galen.schreck@gmail.com>
2025-04-15 08:42:04 -0400

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
