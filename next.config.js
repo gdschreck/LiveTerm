51eadacc8dbe5eefa9759588b23cec64c344c1c1
Galen Schreck <galen.schreck@gmail.com>
2026-06-25 14:13:57 -0400

    Update next.config.js
    Add playbook asset to site

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/playbook',
+        destination:
+          'https://schreckworks-assets.s3.us-east-1.amazonaws.com/AI%20Playbook%20Infographic.png',
+      },
+    ];
+  },
+};
